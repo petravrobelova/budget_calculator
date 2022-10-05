6b2b7fa152e9c6782f93e74edb147cd8327b4f73
Petra Vrobelova <petravrobelova98@gmail.com>
2022-10-05 12:48:10 +0200

    Updates

---
+++ precache-manifest.55167a0fa35df76739fa8f97037ed8e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c23096815e4f07967b15d6a4b9e0a935",
+    "url": "/budget_calculator/index.html"
+  },
+  {
+    "revision": "49fd77a47ac95f496422",
+    "url": "/budget_calculator/static/css/main.ca009a19.chunk.css"
+  },
+  {
+    "revision": "e66f81caa7feec9881d3",
+    "url": "/budget_calculator/static/js/2.4977c8f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/budget_calculator/static/js/2.4977c8f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49fd77a47ac95f496422",
+    "url": "/budget_calculator/static/js/main.f5c0388d.chunk.js"
+  },
+  {
+    "revision": "37a5175d6bdd5af3bc28",
+    "url": "/budget_calculator/static/js/runtime-main.b460c3af.js"
+  }
+]);
